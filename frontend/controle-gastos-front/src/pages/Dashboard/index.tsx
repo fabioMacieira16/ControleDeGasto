742165f8096166a85dbf92bb2747dca923371cf2
Fabio Macieira <fabiomacieira16@gmail.com>
2026-04-13 17:08:58 -0300

    feature: create base page

---
+++ frontend/controle-gastos-front/src/pages/Dashboard/index.tsx
@@ -0,0 +1,9 @@
+export default function Dashboard() {
+    return (
+        <div>
+            <h1>Dashboard</h1>  
+            <p>Bem-vindo ao seu painel de controle de gastos!</p>
+            <p>Aqui você pode visualizar um resumo das suas finanças, acompanhar suas despesas e receitas, e acessar relatórios detalhados.</p>
+        </div>
+    );
+}
